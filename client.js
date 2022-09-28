a1b6bdc13f2f6155061cfce098bf3461522c5632
JillianAubrey <jillian.nk.aubrey@gmail.com>
2022-09-28 14:13:11 +0000

    Separated connection into its own module, and send name to game server

---
+++ client.js
@@ -0,0 +1,30 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function() {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write('Name: JA');
+  });
+
+  conn.on('data', (data) => {
+    console.log(data.toString());
+    conn.end();
+  });
+
+  return conn;
+};
+
+module.exports =  {
+  connect,
+}
+console.log("Connecting ...");
+connect();
